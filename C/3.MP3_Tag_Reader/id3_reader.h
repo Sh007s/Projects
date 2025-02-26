b259d17c55c1413662b8fcaefeab625a33d3aead
Shankar s <shankar007skr@gmail.com>
2025-02-26 20:24:40 +0530

    Update the ID3_Reader.h

--- C/3.MP3_Tag_Reader/id3_reader.h
+++ C/3.MP3_Tag_Reader/id3_reader.h
@@ -15,7 +15,15 @@
 #define ID3V2_HEADER_SIZE 10
 #define MAX_FRAME_SIZE 1048576
 #define MAX_STRING_LENGTH 1024
-#define SAFE_FREE(p) do { if (p) { free(p); p = NULL; } } while (0)
+#define SAFE_FREE(p)  \
+    do                \
+    {                 \
+        if (p)        \
+        {             \
+            free(p);  \
+            p = NULL; \
+        }             \
+    } while (0)
 
 struct ID3v2_header
 {
@@ -26,17 +34,16 @@ struct ID3v2_header
 };
 
 TagData *read_id3v1_tags(const char *filename);
-
 TagData *read_id3v2_tags(const char *filename);
-
 void display_metadata(const TagData *data);
-
 void view_tags(const char *filename);
-
-unsigned int decode_synchsafe(const unsigned char bytes[4]);
-unsigned int is_korean_text(const unsigned char *buf, size_t len);
+void clean_string(char *str);
 char *try_korean_encodings(const char *input, size_t input_len);
 char *get_frame_content(const char *buffer, int size);
 char *get_comment_content(const char *buffer, int size);
+char *normalize_genre(const char *raw_genre);
+char *convert_encoding(const char *input, size_t input_len, const char *from_charset);
+unsigned int decode_synchsafe(const unsigned char bytes[4]);
+unsigned int is_korean_text(const unsigned char *buf, size_t len);
 
 #endif // ID3_READER_H
